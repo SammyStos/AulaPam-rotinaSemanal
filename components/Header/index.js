d5d7449277266a4c1a46461dfe6f881d04d7314e
Samantha Paula <167320236+SammyStos@users.noreply.github.com>
2024-05-03 22:38:18 -0300

    index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -5,7 +5,7 @@ export default function Header(){
     return(
         <>
             <Text style={estilo.titulo}>Minha Rotina Semanal</Text>
-           
+           <Text style={estilo.subtitulo}>Rafa e Sammy</Text>
              <View style={{width:'95%', flexDirection: 'row', marginTop: 20, alignItems:'center', justifyContent:"space-between" }}>
             <Text style={estilo.tituloCard}>Atividade</Text>
             <Text style={estilo.tituloCard}>Descrição</Text>
